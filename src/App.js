9e3281f3d4eaf8450c2a38120afefae1a2cec46f
Aryan <64796509+aryanranderiya@users.noreply.github.com>
2024-03-08 19:41:50 +0530

    build(deps): update package-lock.json, package.json, App.js and src/index.css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import "./App.css";
-
-function Input({ placeholder_text }) {
-  return <input type="text" placeholder={placeholder_text}></input>;
-}
+import React, { useState } from "react";
+import axios from "axios";
 
 function Form() {
+  const [appData, setAppData] = useState({
+    shortURL: "",
+    longURL: "",
+  });
+
+  const onSubmit = (e) => {
+    axios
+      .post("/connect", appData)
+      .then((data) => console.log(data.data))
+      .catch((error) => console.log(error));
+  };
+
+  const onChangeData = (e) => {
+    setAppData({
+      ...appData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <>
       <h1>URL Shortener</h1>
-      <form className="form">
-        <Input placeholder_text="Enter long URL to shorten" />
-        <Input placeholder_text="Enter short url code" />
+      <form className="form" onSubmit={() => onSubmit}>
+        <input
+          type="url"
+          placeholder="Enter long URL to shorten:"
+          value={appData.longURL}
+          name="longURL"
+          onChange={onChangeData}
+          required
+        ></input>
+
+        <input
+          type="url"
+          placeholder="Enter short URL:"
+          value={appData.shortURL}
+          name="shortURL"
+          onChange={onChangeData}
+          required
+        ></input>
+
+        <input type="submit" className="submit_btn"></input>
       </form>
     </>
   );
 }
 
 function App() {
-  return (
-    <>
-      <Form />
-    </>
-  );
+  return <Form />;
 }
 
 export default App;
