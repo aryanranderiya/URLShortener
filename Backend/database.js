4ae5c46c868262ebcc284393c244a1a441751922
Aryan <64796509+aryanranderiya@users.noreply.github.com>
2024-03-10 19:21:26 +0530

    Add Backend to Repo

---
+++ Backend/database.js
@@ -0,0 +1,102 @@
+const mongoose = require("mongoose");
+
+const linksSchema = new mongoose.Schema({
+  shortURL: { type: String, required: true, unique: true, index: true },
+  longURL: { type: String, required: true },
+  createdAt: { type: Date },
+  expireAt: { type: Date },
+});
+
+linksSchema.index({
+  shortURL: "text",
+});
+
+const Link = mongoose.model("linksModel", linksSchema);
+
+const connectDatabase = async () => {
+  const connectionString = process.env.MONGO_DB;
+  try {
+    await mongoose.connect(connectionString);
+    console.log("Successfully Connected!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const insertIntoDatabase = async (
+  shortURL,
+  longURL,
+  expireAfterSeconds = null
+) => {
+  connectDatabase();
+  createIndex();
+  const [currentTime, expiryTime] = await calculateTime(expireAfterSeconds);
+
+  console.log("current time", currentTime);
+  console.log("expiration time", expiryTime);
+
+  try {
+    let link = new Link({
+      shortURL: shortURL,
+      longURL: longURL,
+      createdAt: currentTime,
+      expireAt: expiryTime,
+    });
+
+    await link.save();
+
+    console.log(`Successfully Inserted short URL "${shortURL}"!`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const searchDatabase = async (query) => {
+  try {
+    connectDatabase();
+
+    console.log("Searching Database...", query);
+    const result = await Link.findOne({ shortURL: query });
+
+    console.log(
+      result
+        ? `Search result ${query} found in database!`
+        : `${query} not found! Returning NULL.`
+    );
+
+    return result ? result.longURL : null;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+};
+
+async function createIndex() {
+  const exists = await Link.collection.indexExists("expireAt");
+
+  if (!exists) {
+    await Link.collection.createIndex(
+      { expireAt: 1 },
+      { expireAfterSeconds: 0 },
+      { background: true }
+    );
+  }
+}
+
+async function calculateTime(expireAfterSeconds) {
+  console.log(expireAfterSeconds);
+
+  var expiryTime = null;
+  const currentTime = new Date();
+
+  if (expireAfterSeconds !== null) {
+    console.log("inside cond");
+    expireAfterSeconds = parseInt(expireAfterSeconds);
+    expiryTime = new Date(currentTime.getTime() + expireAfterSeconds * 1000);
+  }
+
+  const object = [currentTime, expiryTime];
+  return object;
+}
+
+module.exports = { connectDatabase, insertIntoDatabase, searchDatabase };
---
+++ Backend/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const { insertIntoDatabase, searchDatabase } = require("./database");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, (error) => {
+  if (error) console.log("Server Could not Start!" + error);
+  else console.log("Server successfully started! Listening on port " + PORT);
+});
+
+app.post("/insert", async (req, res) => {
+  const { shortURL, longURL, expireAfterSeconds } = req.body;
+
+  console.log("Short URL: ", shortURL);
+  console.log("Long URL: ", longURL);
+  console.log("Expiration Time: ", expireAfterSeconds);
+
+  const searchResult = await searchDatabase(shortURL);
+
+  if (searchResult === null) {
+    console.log("No Redundant Data Found...Inserting Into Database!");
+    insertIntoDatabase(shortURL, longURL, expireAfterSeconds);
+    res.end();
+  } else {
+    res
+      .status(409)
+      .json({ error: "Short Link Already Exists! Please try again." });
+  }
+});
+
+app.get("/l/*", async (req, res) => {
+  const url = req.params[0];
+  console.log("URL Parameter:", url);
+  console.log("URL Parameter Type:", typeof url);
+
+  if (url && url !== "/") {
+    const longURL = await searchDatabase(url);
+    console.log(longURL);
+
+    if (longURL !== null) res.redirect(longURL);
+    else res.redirect("https://aryanranderiya.com/404");
+  } else {
+    res.redirect("https://aryanranderiya.com/404");
+  }
+});
